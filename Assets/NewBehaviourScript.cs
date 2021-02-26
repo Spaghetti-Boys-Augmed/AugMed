4ac29f9954d67b4d66939ed2de79ab972a80ac05
guilherme-jafar <gjafar14@gmail.com>
2021-02-26 18:04:58 +0000

    mudar de scenes

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public Button goToSceneRegisto;
+    public int btnValue;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = goToSceneRegisto.GetComponent<Button>();
+        btn.onClick.AddListener(changeScene);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void changeScene()
+    {
+        
+        SceneManager.LoadScene("Scenes/Registo", LoadSceneMode.Single);
+    }
+}
