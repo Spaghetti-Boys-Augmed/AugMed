c8ed3ae7021e977229c6eebd2426e7c1b2dd9614
fabian-nunes <fabiannunes12@gmail.com>
2021-02-27 10:16:10 +0000

    All shit

---
+++ Assets/SimpleSkeleton/Readme_SimpleSkeleton.txt
@@ -0,0 +1,21 @@
+Simple Skeleton is a humanoid-compliant character model. 
+The model has 26771 vertices, 34264 polygons, and 2k texture size. 
+The following animations are included with this model.
+
+ Waiting
+ Walking (straight, curve)
+ Back walking
+ Running (straight, curve)
+ Jumping
+ Rolling
+ Lying down
+ Fighting pose
+ Punching (Right / Left)
+ Change of direction
+ Knockdown
+
+A controller that plays these animations is also included, and you can immediately experience this character in the demo scene after downloading.
+
+If you have any questions, please send a message to the twitter: @yunoda_3DCG
+
+Thank you!
---
+++ Assets/SimpleSkeleton/script/SimpleSkeletonController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleSkeletonController : MonoBehaviour
+{
+    Animator animator;
+    SkinnedMeshRenderer skinnedMesh;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.L))
+            animator.SetTrigger("lying");
+        else if (Input.GetKeyDown(KeyCode.Space))
+            animator.SetTrigger("jump");
+        else if (Input.GetKeyDown(KeyCode.K))
+            animator.SetTrigger("knockdown");
+        else if (Input.GetKeyDown(KeyCode.Mouse0))
+            animator.SetTrigger("punch_L");
+        else if (Input.GetKeyDown(KeyCode.Mouse1))
+            animator.SetTrigger("punch_R");
+
+        animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+
+        if(Input.GetKey(KeyCode.LeftShift))
+            animator.SetBool("running", true);
+        else
+            animator.SetBool("running", false);
+        if (Input.GetKey(KeyCode.LeftControl))
+            animator.SetBool("sidefix", true);
+        else
+            animator.SetBool("sidefix", false);
+    }
+}
